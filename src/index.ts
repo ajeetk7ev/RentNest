70fc903051b9040ff6fdb77f8010d5c8acbede67
ajeetk7ev <ajeetk8568@gmail.com>
2025-10-16 15:13:48 +0530

    remove prisma and add mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import dbConnect from "./config/db";
 
 const app = express();
 
-
-app.listen(3000, () => {
-    console.log("Server is running at port 3000")
-})
+app.listen(3000, async() => {
+  await dbConnect();
+  console.log("Server is running at port 3000");
+});
